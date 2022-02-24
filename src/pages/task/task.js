c62be062f885174050c5aa6301fd8b54add75e17
wceacses <69900262+wceacses@users.noreply.github.com>
2022-02-24 12:24:36 +0530

    updated hints

--- src/pages/task/task.js
+++ src/pages/task/task.js
@@ -5,27 +5,27 @@ const DATA = [
         qha: [
             {
                 question: 'Something that anyone can see but cant stole?',
-                dataString: '',
+                dataString: 'Jo baatne se badta hai.',
                 correctAnswer: `knowledge`
             },
             {
-                question: 'Who i am , i have eyes but cant see ,i have a mouth but cant talk,have legs but cant walk,who am i?',
-                dataString: 'With little girls play',
+                question: 'Who i am ,i have eyes but cant see ,i have a mouth but cant talk,have legs but cant walk,who am i?',
+                dataString: 'Kya mai zinda hu?',
                 correctAnswer: `doll`
             },
             {
                 question: 'I can write but i am not pen,I can walk but i am not any vehical,I can tik-tok but i am not clock, who am i?',
-                dataString: 'we use for typing',
+                dataString: 'Christopher Latham Sholes',
                 correctAnswer: `typewriter`
             },
             {
                 question: 'Titar ke do aage teetar ,Titar ke do piche teetar, age teetar ,piche teetar ,bolo kitne teetar?',
-                dataString: 'carefully lookeach sentence ..(4 digit)',
+                dataString: 'Find the answer for every phrase.',
                 correctAnswer: `2213`
             },
             {
                 question: 'rhenium ,Nitrogen, calcium  , thoriumm , beryllium ?',
-                dataString: 'what about periodic table.',
+                dataString: 'Rearrange me if you can.',
                 correctAnswer: `caren beth`
             },
         ]
@@ -36,18 +36,18 @@ const DATA = [
         qha: [
             {
                 question: 'Gussa mat kar nao-jawan ,Andhi ho ya tufan,garmi sardi ka nhi bhan ,Bol me',
-                dataString: 'Popular line',
+                dataString: 'Kiske hai ye bol?',
                 correctAnswer: `lal bahadur sastri choak`
             },
             {
                 question: 'Sagar kinare Dil ke pukare vahi hote hai lohe ke machhware..',
-                dataString: 'near sea-shore',
+                dataString: 'Jahaz kaha banaye jate hai',
                 correctAnswer: `dockyard`,
-                optionalAnswer: 'shipYard'
+                optionalAnswer: `shipyard`
             },
             {
                 question: 'Pahad ko Jaye Ram Jab ,Banvas se aaye ram tabhi to rawan sharan me aaye',
-                dataString: 'PLace where ravan lives',
+                dataString: 'Batao Batao konsi jagah thi vo..',
                 correctAnswer: `lanka niwas`
             },
             {
@@ -62,13 +62,13 @@ const DATA = [
         story: ` `,
         qha: [
             {
-                question: 'image',
+                question: 'Guess the PassCode to deactivate the bomb',
                 dataString: '7 letter word.',
                 correctAnswer: `rajveer`
             },
             {
                 question: 'Who had informed Byomkesh about Natasha?',
-                dataString: 'Guess',
+                dataString: 'Who called Byomkesh ?',
                 correctAnswer: `rashmika`,
             },
             {
